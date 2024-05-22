d321df10dc8436102720d9703c3c9f3c80c5c24c
Stark <inventorstark7@gmail.com>
2024-05-22 04:34:52 +0100

    fix: include admin field to register controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -22,7 +22,7 @@ const authUser = asyncHandler(async (req, res) => {
 });
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, isAdmin } = req.body;
 
   const userExists = await User.findOne({ email });
 
@@ -35,6 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
     name,
     email,
     password,
+    isAdmin: isAdmin ? isAdmin : false
   });
 
   if (user) {
